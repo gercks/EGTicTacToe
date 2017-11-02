bc7b2df041347371a8225c6ef1ae32ba395d2905
gercks <elizagercke@gmail.com>
2017-11-02 15:59:47 -0400

    Add basic js functionality
    also added a bunch of not-yet-necessary api stuff

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,21 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
+// const gameEvents = require('./games/events')
+
+// $(() => {
+//   $('td').on('click', gameEvents.addX)
+// })
+
+$('td').click(function () {
+  // console.log($(this).html())
+  if (($(this).html() !== 'x') && ($(this).html() !== 'o')) {
+    $(this).html('x')
+    // $(this).html(changeTurn())
+    // marks.splice(this.id, 1, $(this).html()) // adds the last x or o to the marks array
+    // console.log(marks)
+  }
+})
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 

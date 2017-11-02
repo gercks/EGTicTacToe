0133bf9984a0fecb37a9665dad0c2df7b9c0903f
gercks <elizagercke@gmail.com>
2017-11-02 16:28:54 -0400

    More accidental master branch edits

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -1,21 +1,21 @@
-'use strict'
-
-const gamesApi = require('./api.js')
-const gamesUi = require('./ui.js')
-const getFormFields = require('../../../lib/get-form-fields')
-
-const addX = function (event) {
-  event.preventDefault()
-  // const data = getFormFields(event.target)
-  // const game = data.game
-  // // if (td !== 0) {
-  // gamesApi.show(game.playerX)
-  //   .then(gamesUi.onSuccess)
-  //   .catch(gamesUi.onError)
-  // // } else {
-  console.log('hey')
-}
+// 'use strict'
+//
+// const gamesApi = require('./api.js')
+// const gamesUi = require('./ui.js')
+// const getFormFields = require('../../../lib/get-form-fields')
+//
+// const addX = function (event) {
+//   event.preventDefault()
+//   // const data = getFormFields(event.target)
+//   // const game = data.game
+//   // // if (td !== 0) {
+//   // gamesApi.show(game.playerX)
+//   //   .then(gamesUi.onSuccess)
+//   //   .catch(gamesUi.onError)
+//   // // } else {
+//   console.log('hey')
 // }
-module.exports = {
-
-}
+// // }
+// module.exports = {
+//
+// }
